0fbe8f12e56c1b69679ebf54fc75dacc2b3aa0d2
Amin <veteranglow@gmail.com>
2016-05-23 10:50:43 +0000

    toets am3a

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+       <title>LESS</title> 
+       <link rel="stylesheet/less"  type="text/css" href="./style.less"> 
+       <script src="./less.js/dist/less.js"></script> 
+    </head>
+    <body>
+        <h3>LESS tutorial</h3>
+
+        <ol>
+            <li><a href="intro_less.php">Introductie en setup Less</a></li>
+            <li><a href="table_less.php">Tabel opmaak met Less</a></li> 
+            <li><a href="lessphp_example_compile.php">Less-waarde veranderen met php (serverside)</a></li>  
+            <li><a href="lessphp_example_compileFile.php">Less-waarde veranderen met php (serverside) in een less file</a></li>                                  
+            <li><a href="lessphp_example_compileFile_tocss.php">Less-waarde veranderen met php (serverside) in een less file naar css</a></li>                                  
+        </ol>
+    </body>
+</html>
---
+++ intro_less.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+       <title>LESS</title>  
+       <link rel="stylesheet/less"  type="text/css" href="./style.less"> 
+       <script src="./less.js/dist/less.js"></script>    
+    </head>
+    <body>
+        <h3>Intro LESS</h3>
+        <div id="div1">
+            Dit is een div waarvan de opmaak wordt geregeld door LESS
+        </div>
+        
+        <div id="div2">
+            Dit is de tweede div waarvan de opmaak wordt geregeld door LESS
+        </div>
+    </body>
+</html>
